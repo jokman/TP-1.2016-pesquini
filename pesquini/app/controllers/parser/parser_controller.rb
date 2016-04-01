7cf3dd165e0b1949898d99bf2b301f07e2c56026
anacarolina1 <ana_karolopes@hotmail.com>
2016-04-01 20:48:11 -0300

    Apply style sheet

--- pesquini/app/controllers/parser/parser_controller.rb
+++ pesquini/app/controllers/parser/parser_controller.rb
@@ -8,27 +8,27 @@ class Parser::ParserController < ApplicationController
                                        :build_enterprise, :build_sanction, 
                                        :check_and_save]
 
-  def index
+  def index()
 
   end
 
-  def check_nil_ascii(text)
-    if text.include?("\u0000")
+  def check_nil_ascii( text )
+    if text.include?( "\u0000" )
       return "Não Informado"
     else
-      return text.upcase
+      return text.upcase()
     end
   end
 
-  def check_date(text)
+  def check_date( text )
     begin
-      return text.to_date
+      return text.to_date()
     rescue
       return nil
     end
   end
 
-  def check_and_save(c)
+  def check_and_save( c )
     begin
       c.save!
       c
