9b1eb6a03ed5e202ad2da5cb96ccaa43d75f3086
Ygor Galeno <ygor.tgaleno@gmail.com>
2016-04-03 04:15:25 -0300

    Adding the header on file: welcome_controller

--- pesquini/app/controllers/welcome_controller.rb
+++ pesquini/app/controllers/welcome_controller.rb
@@ -1,3 +1,9 @@
+# File: welcome_controller.rb
+# Purpose: Contains search method
+# License: GPL v3.
+# Pesquini Group 6
+# FGA - UnB Faculdade de Engenharias do Gama - University of Brasilia.
+
 class WelcomeController < ApplicationController
   def index
     params[:q][:cnpj_eq] = params[:q][:corporate_name_cont] unless params[:q].nil?
