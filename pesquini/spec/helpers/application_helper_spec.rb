3717ec70faf99c64ed071d3f1f2f3ef40a729e0c
Ygor Galeno <ygor.tgaleno@gmail.com>
2016-04-05 10:28:03 -0300

    Applying and patching the style sheet on file from spec/helper

--- pesquini/spec/helpers/application_helper_spec.rb
+++ pesquini/spec/helpers/application_helper_spec.rb
@@ -1,28 +1,34 @@
-# File: application_helper_spec.rb
-# Purpose: Contains unit test of class ApplicationHelper
-# License: GPL v3.
-# Pesquini Group 6
-# FGA - UnB Faculdade de Engenharias do Gama - University of Brasilia.
-
+=begin
+File: application_helper_spec.rb
+Purpose: Contains unit test from class ApplicationHelper
+License: GPL v3.
+Pesquini Group 6
+FGA - UnB Faculdade de Engenharias do Gama - University of Brasilia.
+=end
 require 'rails_helper'
   
 include ApplicationHelper
 
 RSpec.describe ApplicationHelper, :type => :helper do
+  
   describe '#flash_class' do
+  
     it "should return alert alert-info" do
       expect(flash_class(:notice)).to eq('alert alert-info')
-      end
+    end
 
-       it "should return alert alert-success" do
+    it "should return alert alert-success" do
       expect(flash_class(:success)).to eq('alert alert-success')
     end
-     it "should return alert alert-error" do
+  
+    it "should return alert alert-error" do
       expect(flash_class(:error)).to eq('alert alert-error')
     end
-     it "should return alert alert-error" do
+    
+    it "should return alert alert-error" do
       expect(flash_class(:alert)).to eq('alert alert-error')
     end
+  
   end
 
 end
