60a9723477d2b986af5bc95db8f76c7f731189bf
Ygor Galeno <ygor.tgaleno@gmail.com>
2016-04-05 10:40:29 -0300

    Applying the style sheet on files from spec/models

--- pesquini/spec/models/enterprise_spec.rb
+++ pesquini/spec/models/enterprise_spec.rb
@@ -1,9 +1,17 @@
+=begin
+File: enterprise_spec.rb
+Purpose: Contains a unit test from class Enterprise.
+License: GPL v3.
+Pesquini Group 6
+FGA - UnB Faculdade de Engenharias do Gama - University of Brasilia.
+=end
 require 'spec_helper'
 require 'rails_helper'
 
 describe Enterprise do
 
   before do
+
     @enterprise = Enterprise.new
     @sanction = Sanction.new
     @payment = Payment.new
@@ -18,6 +26,7 @@ describe Enterprise do
     @payment.sign_date = "01/02/2011".to_date
     @payment.enterprise_id = @enterprise.id
     @payment.save
+
   end
   
   subject { @enterprise }
@@ -28,28 +37,34 @@ describe Enterprise do
   describe "uniqueness validation of cnpj" do
     
     describe "unique cnpj" do
+
       it "should be_valid" do
         uniqueness_enterprise = Enterprise.new
         uniqueness_enterprise.cnpj = "1234" 
         expect(uniqueness_enterprise).to be_valid
       end
+
     end
     
-  describe "duplicated cnpj" do
-      it "should not be_valid" do
-        duplicated_enterprise = Enterprise.new
-        duplicated_enterprise.cnpj = "555"
-        expect(duplicated_enterprise).not_to be_valid
-      end
-    end
+  	describe "duplicated cnpj" do
+    
+    	it "should not be_valid" do
+    	  duplicated_enterprise = Enterprise.new
+    	  duplicated_enterprise.cnpj = "555"
+    	  expect(duplicated_enterprise).not_to be_valid
+    	end
+  
+  	end
 
     describe "#payment_after_sanction" do
+    
       it "should return false if have any sanction or any payment" do
         e = Enterprise.new
         expect(e.sanctions.count).to be(0)
         expect(e.payments.count).to be(0)
         expect(e.payment_after_sanction?).to be false
       end
+    
       it "should return false if don't have payment after sanction" do
         expect(@enterprise.payment_after_sanction?).to be false
       end
@@ -59,23 +74,28 @@ describe Enterprise do
         @sanction.save
         expect(@enterprise.payment_after_sanction?).to be true
       end
+    
     end
 
     describe "#self.enterprise_position" do
+      
       it "should return 1 if there is only 1 enterprise" do
         e = Enterprise.new
         e.sanctions_count = 10000
         e.save
-
         expect(Enterprise.enterprise_position(e)).to eq(1);
       end
+   
     end
 
     describe "#refresh!" do
+      
       before do
+      
         @e = Enterprise.new
         @e.cnpj = "12575431567543"
         @e.save
+      
       end
 
       it "should return enterprise" do
@@ -85,6 +105,9 @@ describe Enterprise do
       it "should not return other enterprise" do
         expect(@e.refresh!).not_to eq(@enterprise);
       end  
+   
     end
+  
   end   
+
 end
--- pesquini/spec/models/sanction_spec.rb
+++ pesquini/spec/models/sanction_spec.rb
@@ -1,12 +1,21 @@
+=begin
+File: sanction_spec.rb
+Purpose: Contains a unit test from class Sanction.
+License: GPL v3.
+Pesquini Group 6
+FGA - UnB Faculdade de Engenharias do Gama - University of Brasilia.
+=end
 require 'spec_helper'
 require 'rails_helper'
 
 describe Sanction do
 
   before do
+
     @sanction = Sanction.new
     @sanction.process_number = "9090"
     @sanction.save
+
   end
   
   subject { @sanction }
@@ -14,37 +23,48 @@ describe Sanction do
   it { should be_valid }
 
   describe 'percentual_sanction' do
+
     it " to do work" do
       b = Sanction.percentual_sanction(10000)
       c = b.to_i
       expect(c).to eq(188)
     end
+
   end
 
   describe "uniqueness validation of process_number" do
     
     describe "unique process_number" do
+
       it "should be_valid" do
         uniqueness_sanction = Sanction.new
         uniqueness_sanction.process_number = "123" 
         expect(uniqueness_sanction).to be_valid
-          end
-        end
+      end
+    
+    end
+    
     describe "duplicated process_number" do
+    
       it "should not be_valid" do
         duplicated_sanction = Sanction.new
         duplicated_sanction.process_number = "9090" 
         expect(duplicated_sanction).not_to be_valid
       end
+    
     end
   
 
     describe "#refresh!" do
+    
       before do
+    
         @s = Sanction.new
         @s.process_number = "356754"
         @s.save
+    
       end
+    
       it "should return sanction" do
         expect(@s.refresh!).to eq(@s);
       end
@@ -52,6 +72,7 @@ describe Sanction do
       it "should not return other sanction" do
         expect(@s.refresh!).not_to eq(@sanction);
       end  
+    
     end
 
   end   
