562854f6619733f9497fcf372f4fe6adf8efb47d
anacarolina1 <ana_karolopes@hotmail.com>
2016-04-05 17:38:35 -0300

    Apply style sheet

--- pesquini/app/models/sanction_type.rb
+++ pesquini/app/models/sanction_type.rb
@@ -1,42 +1,46 @@
 =begin
-File: user_controller.rb
-Purpose: Contains validates description of sanction.
+File: sanction_type.rb
+Purpose: Class to defines the types of sanctions..
 License: GPL v3.
 Pesquini Group 6
 FGA - UnB Faculdade de Engenharias do Gama - University of Brasilia.
 =end
+
 class SanctionType < ActiveRecord::Base
 
   has_many :sanctions
   validates_uniqueness_of :description
 
-  def refresh!
+  def refresh!()
 
-    s = SanctionType.find_by_description(self.description)
+    s = SanctionType.find_by_description( self.description )
 
   end
 
 
-  def self.all_sanction_types
+  def self.all_sanction_types()
 
     stantion_types = [
- [ "INIDONEIDADE - LEGISLAçãO ESTADUAL", "Inidoneidade - Legislação Estadual"],
- [ "IMPEDIMENTO - LEI DO PREGãO", "Impedimento - Lei do Pregão"],
- [ "PROIBIçãO - LEI ELEITORAL", "Proibição - Lei Eleitoral"],
- [ "INIDONEIDADE - LEI DE LICITAçõES","Inidoneidade - Lei de Licitações"],
- [ "SUSPENSãO - LEI DE LICITAçõES","Suspensão - Lei de Impedimento Licitações"],
- [ "SUSPENSãO - LEGISLAçãO ESTADUAL", "Suspensão - Legislação estadual"],
- [ "PROIBIçãO - LEI DE IMPROBIDADE", "Proibição - Lei de improbidade"],
- [ "DECISãO JUDICIAL LIMINAR/CAUTELAR QUE IMPEçA CONTRATAçãO","Decisão Judicial liminar"] ,
- [ "INIDONEIDADE - LEI DA ANTT E ANTAQ ","Inidoneidade - Lei da ANTT e ANTAQ"] ,
- [ "INIDONEIDADE - LEI ORGâNICA TCU", "Inidoneidade - Lei Orgânica TCU"],
- [ "IMPEDIMENTO - LEGISLAçãO ESTADUAL", "Impedimento - Legislação Estadual"],
- [ "SUSPENSãO E IMPEDIMENTO - LEI DE ACESSO à INFORMAçãO","Suspensão e Impedimento - Lei de Acesso à Informação"],
- [ "PROIBIçãO - LEI ANTITRUSTE", "Proibição - Lei Antitruste"],
- [ "IMPEDIMENTO - LEI DO RDC", "Impedimento - Lei do RDC"],
- [ "PROIBIçãO - LEI AMBIENTAL", "Proibição - Lei Ambiental" ],
- ]
- stantion_types
+      [ "INIDONEIDADE - LEGISLAçãO ESTADUAL", "Inidoneidade - Legislação Estadual"],
+      [ "IMPEDIMENTO - LEI DO PREGãO", "Impedimento - Lei do Pregão"],
+      [ "PROIBIçãO - LEI ELEITORAL", "Proibição - Lei Eleitoral"],
+      [ "INIDONEIDADE - LEI DE LICITAçõES", "Inidoneidade - Lei de Licitações"],
+      [ "SUSPENSãO - LEI DE LICITAçõES","Suspensão - Lei de Impedimento Licitações"],
+      [ "SUSPENSãO - LEGISLAçãO ESTADUAL", "Suspensão - Legislação estadual"],
+      [ "PROIBIçãO - LEI DE IMPROBIDADE", "Proibição - Lei de improbidade"],
+      [ "DECISãO JUDICIAL LIMINAR/CAUTELAR QUE IMPEçA CONTRATAçãO", 
+      	"Decisão Judicial liminar"],
+      [ "INIDONEIDADE - LEI DA ANTT E ANTAQ ", "Inidoneidade - Lei da ANTT e ANTAQ"],
+      [ "INIDONEIDADE - LEI ORGâNICA TCU", "Inidoneidade - Lei Orgânica TCU"],
+      [ "IMPEDIMENTO - LEGISLAçãO ESTADUAL", "Impedimento - Legislação Estadual"],
+      [ "SUSPENSãO E IMPEDIMENTO - LEI DE ACESSO à INFORMAçãO", 
+      	"Suspensão e Impedimento - Lei de Acesso à Informação"],
+      [ "PROIBIçãO - LEI ANTITRUSTE", "Proibição - Lei Antitruste"],
+      [ "IMPEDIMENTO - LEI DO RDC", "Impedimento - Lei do RDC"],
+      [ "PROIBIçãO - LEI AMBIENTAL", "Proibição - Lei Ambiental" ],
+    ]
+
+    stantion_types
 
   end
 
--- pesquini/app/models/state.rb
+++ pesquini/app/models/state.rb
@@ -5,22 +5,23 @@ License: GPL v3.
 Pesquini Group 6
 FGA - UnB Faculdade de Engenharias do Gama - University of Brasilia.
 =end
+
 class State < ActiveRecord::Base
 
   has_many :sanctions
   validates_uniqueness_of :abbreviation
 
-  def refresh!
+  def refresh!()
 
-    s = State.find_by_abbreviation(self.abbreviation)
+    s = State.find_by_abbreviation( self.abbreviation )
 
   end
 
-  def self.all_states
+  def self.all_states()
 
     states = ["BA", "DF", "RJ", "PA", "MG", "SP", "AM", "RS", "SC", "ES", "PR",
-     "PB", "RN", "CE", "AL", "RR", "SE", "RO","PI" , "AC",
-    "TO", "GO", "PE", "AP", "MS", "MT", "MA","Não Informado"]
+              "PB", "RN", "CE", "AL", "RR", "SE", "RO","PI" , "AC",
+              "TO", "GO", "PE", "AP", "MS", "MT", "MA","Não Informado"]
     states
 
   end
