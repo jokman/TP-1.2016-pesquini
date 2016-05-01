38e9ad3c127836b0d5aeaa07ec53de56845bb172
anacarolina1 <ana_karolopes@hotmail.com>
2016-04-30 23:48:35 -0300

    Comments following api standard.

--- pesquini/app/controllers/sessions_controller.rb
+++ pesquini/app/controllers/sessions_controller.rb
@@ -8,16 +8,30 @@ FGA - UnB Faculdade de Engenharias do Gama - University of Brasilia.
 
 class SessionsController < ApplicationController
 
+  # 
+  # Empty method.
+  # 
+  # @return 
   def new()
 
   end
 
+  # 
+  # Method that create a session.
+  # 
+  # @return logged status.
   def create()
 
     Preconditions.check_not_nil( login )
     Preconditions.check_not_nil( password )
+
+    # [String] Receive the parameters to login.
     login = params[:session][:login].downcase
+
+    # [String] Keep a password for a determined login.
     password = params[:session][:password]
+
+    # [String] Keep the logged user.
     user = User.find_by( login: login )
 
     if user && user.authenticate( password )
@@ -30,6 +44,10 @@ class SessionsController < ApplicationController
 
   end
 
+  # 
+  # Method that destroy the session.
+  # 
+  # @return loggout status.
   def destroy()
 
     assert user.empty?, "User must not be empty!"
