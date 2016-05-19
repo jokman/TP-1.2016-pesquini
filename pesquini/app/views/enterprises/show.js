0d5cc398315a1cc490a18acf7e06f2ae1092af17
Cristiano Costa <engcristianoc@gmail.com>
2016-05-19 17:40:16 -0300

    Adding headers to html files

--- pesquini/app/views/enterprises/show.js
+++ pesquini/app/views/enterprises/show.js
@@ -1,3 +1,10 @@
+/*
+File: show.js
+Purpose: Provides javascript structures for sanction.
+License: GPL v3.
+Pesquini Group 6
+FGA - UnB Faculdade de Engenharias do Gama - University of Brasilia.
+*/
 
   var sanctions_btn = document.getElementById("sanction-info-btn");
 
