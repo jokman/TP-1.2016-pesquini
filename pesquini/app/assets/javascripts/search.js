bd1f8b0c4cb1f651e671a47ccfa8c40d8928af4d
Ygor Galeno <ygor.tgaleno@gmail.com>
2016-06-14 22:37:45 -0300

    Adding scripts to be loaded on enterprise page

--- pesquini/app/assets/javascripts/search.js
+++ pesquini/app/assets/javascripts/search.js
@@ -6,16 +6,15 @@ Pesquini Group 6
 FGA - UnB Faculdade de Engenharias do Gama - University of Brasilia.
 */
 
- $(document ).ready(function ( ) {
+//= require search.js
+$(document ).ready(function ( ) {
   var element = document.getElementById('enterprises_index');
-  if (element)
-  {
-  var element1 = element.getElementsByClassName("fa_search")[0];
-  var search1 = element.getElementsByClassName("search_form_enterprises")[0];
+  if (element)  {
+    var element1 = element.getElementsByClassName("fa_search")[0];
+    var search1 = element.getElementsByClassName("search_form_enterprises")[0];
   }
   $(element).on('click', function () {
     $(search1).removeClass("off");
     $(element1).addClass('off');
   });
-
 });
