d86d8fb97581d1dca4f079089510a011504006e8
Ygor Galeno <ygor.tgaleno@gmail.com>
2016-07-01 04:40:42 -0300

    Doing internalization of welcome

--- pesquini/app/controllers/application_controller.rb
+++ pesquini/app/controllers/application_controller.rb
@@ -19,5 +19,15 @@ class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, :with => :render_not_found
   rescue_from ActionController::RoutingError, :with => :render_not_found
 
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
 
 end
