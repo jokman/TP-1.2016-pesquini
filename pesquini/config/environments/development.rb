305987e622b54552902defa7a0a03e9dce499d2b
Ygor Galeno <ygor.tgaleno@gmail.com>
2016-07-01 01:18:32 -0300

    Setting enviroment of internalization

--- pesquini/config/environments/development.rb
+++ pesquini/config/environments/development.rb
@@ -19,7 +19,7 @@ Rails.application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
-  # Raise an error on page load if there are pending migrations.
+  # Raise an error on page load if there are pending migrations.e
   config.active_record.migration_error = :page_load
 
   # Debug mode disables concatenation and preprocessing of assets.
@@ -38,4 +38,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.i18n.available_locales = ['pt-BR', :en]
 end
